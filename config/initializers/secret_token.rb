8efa3636b11167aca72419d39b9fabc7e4af8adb
Glenn Goodrich <glenn.goodrich@gmail.com>
2012-10-07 09:50:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnockoutJsRails::Application.config.secret_token = 'd1a60313c79b35ef35b02b05a6e78a0b302a1875855851e8cb426770a619c2177728a68f568b4d8545a95cd3b61c8625778c69765bb444c984b72abcf725eec4'
